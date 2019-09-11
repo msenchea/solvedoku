707af5192bc6442eab1514c4dc13c06acad517bf
xequals1 <marius.senchea2@mail.dcu.ie>
2019-09-11 18:20:25 +0100

    data initialisation

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import sys
+import functions
 
 data = sys.stdin.read().strip().split()
 
 data = ''.join(data)
 
-#set of 3 columns, starting step in range 0-2.
-for i in range(1, 81, 9):
-    print(data[i], data[i + 1], data[i + 2])
+#data initialisation
+v_lines = []
+h_lines = []
 
+j = 0
+while j < 9:
+    temp = []
+    for i in range(j, 81, 9):
+        temp.append(data[i])
+    h_lines.append(temp)
+    j += 1
 
+j = 1
+while j <= 9:
+    temp = []
+    for i in range((j - 1) * 9, 9 * j, 1):
+        temp.append(data[i])
+    v_lines.append(temp)
+    j += 1
 
-
+print(v_lines)
+print(h_lines)
